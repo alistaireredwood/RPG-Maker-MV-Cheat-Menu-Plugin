2d53f4468cc45963f54e50bd4c648024978cf6a2
Wiktor Jagiełło <wiktor.o.jagiello@gmail.com>
2025-05-09 18:59:01 +0200

    feat(vite): remove sprites folder

--- src/types/rpgmaker/mv/scenes/Scene_Save.d.ts
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------------------------------
-// Scene_Save
-//
-// The scene class of the save screen.
-declare class Scene_Save extends Scene_File
-{
-	public onSaveSuccess():void;
-	public onSaveFailure():void;
-}

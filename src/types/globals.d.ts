ce5a23947029e9d380e66c4a8e9d8a44808a068f
Wiktor Jagiełło <wiktor.o.jagiello@gmail.com>
2025-05-10 13:51:17 +0200

    feat(fix-ts-errors): refactor

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -6,7 +6,7 @@ declare interface ICheatMenu {
   menu_update_timer: NodeJS.Timeout;
 
   cheat_selected: number;
-  cheat_selected_actor: number; // This is an Actor ID
+  cheat_selected_actor: number;
   amounts: number[];
   amount_index: number;
 
@@ -26,26 +26,17 @@ declare class Cheat_Menu_Game_Actor extends Game_Actor {
   god_mode?: boolean;
   god_mode_interval?: NodeJS.Timeout;
 
-  /** Backup of Game_Battler.prototype.gainHp */
   gainHP_bkup?: (value: number) => void;
-
-  /** Backup of Game_BattlerBase.prototype.setHp */
   setHp_bkup?: (hp: number) => void;
 
-  /** Backup of Game_Battler.prototype.gainMp */
   gainMp_bkup?: (value: number) => void;
-
-  /** Backup of Game_BattlerBase.prototype.setMp */
   setMp_bkup?: (mp: number) => void;
 
-  /** Backup of Game_Battler.prototype.gainTp */
   gainTp_bkup?: (value: number) => void;
-
-  /** Backup of Game_BattlerBase.prototype.setTp */
   setTp_bkup?: (tp: number) => void;
 
   /** Backup of Game_BattlerBase.prototype.paySkillCost */
-  // paySkillCost_bkup: (skill: DataSkill) => void; // Use DataSkill or RPG.Skill based on your RMMV types
+  // paySkillCost_bkup: (skill: DataSkill) => void;
 }
 
 declare class Game_System {
@@ -53,11 +44,11 @@ declare class Game_System {
 }
 
 declare class DataManager {
-  static default_loadGame?: (savefileId: number) => boolean;
+  static default_loadGame?: (saveFileId: number) => boolean;
   static default_setupNewGame?: () => void;
-  static default_saveGame?: (savefileId: number) => boolean;
+  static default_saveGame?: (saveFileId: number) => boolean;
   // don't know why I needed to re-declare those
-  static loadGame: ((savefileId: number) => boolean) | undefined;
+  static loadGame: ((saveFileId: number) => boolean) | undefined;
   static setupNewGame: () => void;
-  static saveGame: ((savefileId: number) => boolean) | undefined;
+  static saveGame: ((saveFileId: number) => boolean) | undefined;
 }
